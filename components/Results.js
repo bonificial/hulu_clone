a675aaec14ceb40ba60179a8a447512e61303047
bonificial <mboniface107@gmail.com>
2021-10-31 19:55:32 +0300

    Forwardref error fixed

--- components/Results.js
+++ components/Results.js
@@ -5,7 +5,7 @@ import FlipMove from 'react-flip-move';
 function Results({results}) {
     return (
         <FlipMove className="px-5 my-10 sm:grid sm:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
-            {results.map(result=>(<ThumbNail key={result.id} result={result}/>))}
+            {results.map(result=>(<ThumbNail  key={result.id} result={result}/>))}
         </FlipMove>
     )
 }
--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Image from 'next/image'
 import { ThumbUpIcon } from '@heroicons/react/outline'
-import { forwardRef } from 'react'
-function  ThumbNail    ({ result },ref) {
+ 
+function  ThumbNail    ({ result }) {
     const BASE_URL = 'https://image.tmdb.org/t/p/original/'; //////
     return (
-        <div ref={ref} className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+        <div  className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
             <Image alt=""
              src={`${BASE_URL}${result.backdrop_path ||
                 result.poster_path}` ||
@@ -25,4 +25,5 @@ function  ThumbNail    ({ result },ref) {
         </div>
     )
 };
- export default ThumbNail =  React.forwardRef(ThumbNail)
+
+ export default ThumbNail  
