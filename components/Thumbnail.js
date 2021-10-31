0333998543e7a1b0d177202728d49ae9853bb19d
bonificial <mboniface107@gmail.com>
2021-10-31 19:43:17 +0300

    Forwardref error fixed

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { ThumbUpIcon } from '@heroicons/react/outline'
 import { forwardRef } from 'react'
 function  ThumbNail    ({ result },ref) {
-    const BASE_URL = 'https://image.tmdb.org/t/p/original/'
+    const BASE_URL = 'https://image.tmdb.org/t/p/original/'; ///
     return (
         <div ref={ref} className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
             <Image alt=""
@@ -17,7 +17,7 @@ function  ThumbNail    ({ result },ref) {
                 </h2>
                 <p className="flex items-center opacity-0 group-hover:opacity-100">
                     {result.media_type && `${result.media_type} •`}{" "}
-                    {result.release_date || result.first_air_date} •{" "}
+                    {result.release_date || result.first_air_date} •{" "}  
                     <ThumbUpIcon className="h-5 mx-2" />
                      {result.vote_count}
                 </p>
