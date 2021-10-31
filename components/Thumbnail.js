856f9dc760b95383f2a37daac8ae22b0857ae3bb
bonificial <mboniface107@gmail.com>
2021-10-31 20:44:16 +0300

    Page Updated

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { ThumbUpIcon } from '@heroicons/react/outline'
  
 export default function  ThumbNail ({ result }) {
-    const BASE_URL = 'https://image.tmdb.org/t/p/original/'; ////////
+    const BASE_URL = 'https://image.tmdb.org/t/p/original/'; ///////////
     return (
         <div  className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
             <Image alt=""
